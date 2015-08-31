2cd26d148e5664836eb989128273da96e5a0eeae
unknown <rguluarte@vmartinez.republic.streamsol.com>
2015-08-31 14:16:28 -0700

    first

---
+++ PinterestBotC/PinterestBotC/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PinterestBotC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.geckoWebBrowser = new Gecko.GeckoWebBrowser();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 564);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel.Text = "toolStripStatusLabel1";
+            // 
+            // geckoWebBrowser
+            // 
+            this.geckoWebBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.geckoWebBrowser.Location = new System.Drawing.Point(0, 331);
+            this.geckoWebBrowser.Name = "geckoWebBrowser";
+            this.geckoWebBrowser.Size = new System.Drawing.Size(784, 230);
+            this.geckoWebBrowser.TabIndex = 2;
+            this.geckoWebBrowser.UseHttpActivityObserver = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(697, 302);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(13, 13);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(759, 283);
+            this.txtText.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 586);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.geckoWebBrowser);
+            this.Controls.Add(this.statusStrip);
+            this.Name = "Form1";
+            this.Text = "Google Translate Scrapper";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private Gecko.GeckoWebBrowser geckoWebBrowser;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtText;
+    }
+}
+
---
+++ PinterestBotC/PinterestBotC/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Web;
+using System.Windows.Forms;
+using Gecko;
+using Gecko.Events;
+using HtmlAgilityPack;
+
+namespace PinterestBotC
+{
+    public partial class Form1 : Form
+    {
+        private const string GOOGLE_URL = "https://translate.google.com/#{0}/{1}/{2}";
+        private const string AppName = "PINTEREST_BOT";
+        private string xulPath = "";
+        private string profileDirectory = "";
+        private int scrollNum = 0;
+        private string localAppDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\"+AppName+@"\";
+
+
+        private Queue<string> wordsToScrap = new Queue<string>();
+        private List<string> listWordScrapped = new List<string>(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            xulPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            xulPath = xulPath.Substring(0, xulPath.LastIndexOf(@"\") + 1) + @"xulrunner-sdk\bin";
+
+            Random random = new Random();
+            profileDirectory = ProfileDirectory("DefaultProfile" + 3);
+
+            Xpcom.ProfileDirectory = profileDirectory;
+
+            Xpcom.Initialize(xulPath);
+
+           
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           toolStripStatusLabel.Text = "New profile directory = " + profileDirectory;
+
+        }
+
+        private void geckoWebBrowser_DocumentCompleted(object sender, GeckoDocumentCompletedEventArgs e)
+        {
+            ScrapeElements();
+        }
+
+        private void ScrapeElements()
+        {
+            GeckoHtmlElement element;
+            var geckoDomElement = geckoWebBrowser.Document.DocumentElement;
+            if (geckoDomElement is GeckoHtmlElement)
+            {
+                element = (GeckoHtmlElement)geckoDomElement;
+                var innerHtml = element.InnerHtml;
+
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(innerHtml);
+
+                var translationCollection =
+                    doc.DocumentNode.Descendants("span")
+                        .Where(
+                            d =>
+                                d.Attributes.Contains("id") &&
+                                d.Attributes["id"].Value.Contains("result_box"));
+
+                var translation = translationCollection.FirstOrDefault();
+
+                var otherTranslations =
+                    doc.DocumentNode.Descendants("div")
+                        .Where(
+                            d =>
+                                d.Attributes.Contains("class") &&
+                                d.Attributes["class"].Value.Contains("gt-baf-word-clickable"));
+
+                
+                var moreTranslations =
+                    doc.DocumentNode.Descendants("span")
+                        .Where(
+                            d =>
+                                d.Attributes.Contains("class") &&
+                                d.Attributes["class"].Value.Contains("gt-baf-back"));
+               
+                var seeAlso =
+                   doc.DocumentNode.Descendants("span")
+                       .Where(
+                           d =>
+                               d.Attributes.Contains("class") &&
+                               d.Attributes["class"].Value.Contains("gt-cd-cl"));
+
+                var definitions =
+                   doc.DocumentNode.Descendants("div")
+                       .Where(
+                           d =>
+                               d.Attributes.Contains("class") &&
+                               d.Attributes["class"].Value.Contains("gt-def-row"));
+
+                var examples =
+                   doc.DocumentNode.Descendants("div")
+                       .Where(
+                           d =>
+                               d.Attributes.Contains("class") &&
+                               d.Attributes["class"].Value.Contains("gt-def-example"));
+
+                pushLine(currentWord, translation.InnerText, otherTranslations, moreTranslations,  seeAlso, definitions, examples);
+
+            }
+
+            isGeckoFetching = false;
+        }
+
+
+        private void writeNode(IEnumerable<HtmlNode> examples)
+        {
+            foreach (var htmlNode in examples)
+            {
+                Console.WriteLine(htmlNode.InnerText);
+            }
+        }
+
+        private string getCsvField(IEnumerable<HtmlNode> examples, string separator)
+        {
+            var field = "\"";
+            foreach (var htmlNode in examples)
+            {
+                field += htmlNode.InnerText.Replace("\"", "") + separator;
+            }
+
+            field = field.TrimEnd(' ').TrimEnd(',');
+            field += "\"";
+            return field;
+        }
+
+        private StreamWriter textStream = new StreamWriter(@"output.txt");
+
+        private void pushLine(string word, string trasnlation, IEnumerable<HtmlNode> otherTranslations, IEnumerable<HtmlNode> moreTranslations, IEnumerable<HtmlNode> seeAlso, IEnumerable<HtmlNode> definitions,
+            IEnumerable<HtmlNode> examples)
+        {
+            var field = "";
+
+            field += "\"" + word.Replace("\"", "") + "\",";
+            field += "\"" + trasnlation.Replace("\"", "") + "\",";
+            
+            field += getCsvField(otherTranslations, ", ") + ",";
+            field += getCsvField(moreTranslations, ", ") + ",";
+            field += getCsvField(seeAlso, ", ") + ",";
+            field += getCsvField(definitions, "<br />") + ",";
+            field += getCsvField(examples, "<br />");
+
+
+            textStream.WriteLine(field);
+            textStream.Flush();
+        }
+
+        private string ProfileDirectory(string profileName)
+        {
+            string profileDirectory = localAppDirectory + profileName;
+
+            if (!Directory.Exists(profileDirectory))
+            {
+                Directory.CreateDirectory(profileDirectory);
+            }
+            return profileDirectory;
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private bool isGeckoFetching = false;
+        private string currentWord = String.Empty;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            using (StringReader sr = new StringReader(txtText.Text))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    line = line.ToLower();
+
+                    if (!wordsToScrap.Contains(line) && !string.IsNullOrWhiteSpace(line))
+                    {
+                        wordsToScrap.Enqueue(line);
+
+                        var words = GetWords(line);
+
+                        foreach (var word in words)
+                        {
+                            var normalizedWord = word.ToLower().Replace("qqoottee", "'");
+                            if (!wordsToScrap.Contains(normalizedWord))
+                            {
+                                wordsToScrap.Enqueue(normalizedWord);
+                            }
+                        }
+
+                    }
+                }//while
+
+            }//using
+
+            while (wordsToScrap.Count > 0)
+            {
+                currentWord = wordsToScrap.Peek();
+                toolStripStatusLabel.Text = String.Format("Fetching {0}", currentWord);
+                isGeckoFetching = true;
+                geckoWebBrowser = null;
+                geckoWebBrowser = new GeckoWebBrowser();
+                geckoWebBrowser.DocumentCompleted += geckoWebBrowser_DocumentCompleted;
+                var url = String.Format(GOOGLE_URL, "fr", "en", HttpUtility.UrlEncode(currentWord));
+                geckoWebBrowser.Navigate(url);
+
+                while (isGeckoFetching)
+                {
+                    Application.DoEvents();
+                }
+
+                wordsToScrap.Dequeue();
+            }
+
+            MessageBox.Show("Done");
+
+        }
+
+        static string[] GetWords(string input)
+        {
+            input = input.Replace("'", "qqoottee");
+            input = input.Replace("’", "qqoottee");
+            
+            MatchCollection matches = Regex.Matches(input, @"\b[\w']*\b");
+
+            var words = from m in matches.Cast<Match>()
+                        where !string.IsNullOrEmpty(m.Value)
+                        select TrimSuffix(m.Value);
+
+            return words.ToArray();
+        }
+
+        static string TrimSuffix(string word)
+        {
+            int apostropheLocation = word.IndexOf('\'');
+            if (apostropheLocation != -1)
+            {
+                word = word.Substring(0, apostropheLocation);
+            }
+
+            return word;
+        }
+    }
+}
